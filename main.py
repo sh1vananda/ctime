5b26095b04c8573df428aa658ea4f564e16fc42a
Shivananda Reddy Kankanala <73632304+sh1vananda@users.noreply.github.com>
2024-10-31 22:33:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from get_time_for_location import get_time_for_location
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],  
+)
 
 @app.get("/time")
 async def read_time(country: str, city: str):
